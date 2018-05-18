c37e206caa99a116a5d194f0e16ce8c2117cd21f
Ivan_Seliukov <I>
2018-05-18 16:04:34 +0300

    Implement HorizontalBoxBlur

--- scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -42,21 +42,46 @@ object HorizontalBoxBlur {
    *  Within each row, `blur` traverses the pixels by going from left to right.
    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
-  // TODO implement this method using the `boxBlurKernel` method
-
-  ???
+    // TODO implement this method using the `boxBlurKernel` method
+    for (y <- from until end; x <- 0 until src.width) {
+      dst.update(x, y, boxBlurKernel(src, x, y, radius))
+    }
   }
 
   /** Blurs the rows of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  rows.
-   */
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * rows.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-  // TODO implement using the `task` construct and the `blur` method
+    // TODO implement using the `task` construct and the `blur` method
 
-  ???
+    // 3x3, 1 task, 1 radius
+    val step = src.height / numTasks // 3
+
+    if (step != 0) {
+      val starts = 0 until src.height by step
+      val ends = step - 1 to src.height by step
+
+      var startIdx = 0
+      var endIdx = 0
+
+      while (startIdx < starts.length && endIdx < ends.length) {
+        val t = task {
+          blur(src, dst, starts(startIdx), ends(endIdx), radius)
+        }
+        t.join()
+        startIdx += 1
+        endIdx += 1
+      }
+    } else {
+      val t = task {
+        blur(src, dst, 0, src.height, radius)
+      }
+      t.join()
+    }
+
   }
 
 }
